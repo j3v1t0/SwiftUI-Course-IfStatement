9d5aeb0fcaab2bf11b83868b1f05532036dfb7a5
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-09-22 16:57:27 -0400

    If Statement

---
+++ IfStatement.playground/Contents.swift
@@ -0,0 +1,33 @@
+import PlaygroundSupport
+
+// If Statement
+let age1 = 24
+let age2 = 18
+
+/*
+ Comparison Operators
+ ==
+ ===
+ <=
+ >=
+ !=
+ Logical Operators
+ && AND
+ || OR
+*/
+
+if age1 >= 21{
+    //true
+    print("The user is 21 years old")
+}else{
+    //false
+    print("The user is under 21 years old")
+}
+
+if age2 >= 21{
+    //true
+    print("The user is 21 years old")
+}else{
+    //false
+    print("The user is under 21 years old")
+}
